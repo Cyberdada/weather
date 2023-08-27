a10dca92da97861b43da7c8e18f71b0164ddbe1d
Magnus G <maggud@gmail.com>
2023-08-27 20:26:21 +0200

    trying out tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './domain/weather/dashboard/search/search.component';
